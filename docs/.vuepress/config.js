b53ad6b18414d28c1927cc863ce82c06d0d5b409
YyUuZzHhEe <1308146007@qq.com>
2019-09-25 23:10:42 +0800

    update/product

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,7 +47,7 @@ module.exports = {
         {
           title: "产品",
           collapsable: false,
-          children: ["", "freshmen_fall"]
+          children: ["", "freshmen_fall", "freshmen_spring"]
         }
       ]
     }
